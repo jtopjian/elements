5ae9878834981081ea52bd52c019d7e023218c35
Erich Smith <heartquest@gmail.com>
2017-04-05 23:39:16 -0400

    Use latest metadata endpoint for openstack

--- lib/elements/openstack.go
+++ lib/elements/openstack.go
@@ -1,5 +1,5 @@
 package elements
 
 func (e *Elements) GetOpenStackElements() (map[string]interface{}, error) {
-	return e.GetElementsFromJsonUrl("http://169.254.169.254/openstack/2013-10-17/meta_data.json")
+	return e.GetElementsFromJsonUrl("http://169.254.169.254/openstack/latest/meta_data.json")
 }
